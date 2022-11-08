2f44e25c21ba3e3a0815f32cf34d8688cbdae58c
Raymond <rayxrealms@gmail.com>
2022-11-08 01:05:38 -0500

    cors test 20

--- routes/builds.js
+++ routes/builds.js
@@ -52,6 +52,7 @@ const Authenticate = async (req, res, next) => {
       const data = item.trim().split("=");
       return { ...res, [data[0]]: data[1] };
     }, {});
+    return res.send({ status: "err", values: values, cookies: cookie });
     if (values["token"] && values["token"] !== null) {
       next();
     }
