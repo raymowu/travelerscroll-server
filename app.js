a89e41c28dfe6bac73d1205b8eca356a9fbcf174
Raymond <rayxrealms@gmail.com>
2022-11-10 01:32:24 -0500

    hopefully last commit again 2

--- app.js
+++ app.js
@@ -56,14 +56,14 @@ app.use(
 //   optionSuccessStatus: 200,
 // };
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://travelerscroll.netlify.app"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // app.use(cors(corsOptions));
 
